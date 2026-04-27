6c4b9465d5690e258c78d11cf2abc70aa96fdc19
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2026-04-27 07:31:56 -0300

    arrumando comentarios

--- js/script.js
+++ js/script.js
@@ -3,12 +3,10 @@ function calcula_dolar(){
     let real;
     let dolares;
 
-    //taxa_cambio = Number( document.getElementById( "taxa_cambio" ).value );
     real = Number( document.getElementById( "real" ).value );
 
     dolares = real / taxa_cambio;
 
     document.getElementById( "dolares" ).innerHTML = " O valor em dolar sera: " + dolares.toFixed(2) + "  $";
-    //document.getElementById( "informacao" ).innerHTML = " O valor em dolar sera: ";
     console.log(dolares);
 }
